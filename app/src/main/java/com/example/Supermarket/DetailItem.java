bce56c84bfdfe69e8c944209504b32de4e6afae8
pricyWinner <alex.imanuel.28@gmail.com>
2022-06-16 10:10:56 +0700

    fix cart

--- app/src/main/java/com/example/Supermarket/DetailItem.java
+++ app/src/main/java/com/example/Supermarket/DetailItem.java
@@ -26,6 +26,7 @@ public class DetailItem extends AppCompatActivity {
     private TextView tv_title, tv_desc, tv_price;
     private Button btn_minus, btn_plus;
     private EditText et_count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +38,7 @@ public class DetailItem extends AppCompatActivity {
         tv_price = (TextView) findViewById(R.id.tv_item_price);
         btn_minus = (Button) findViewById(R.id.btn_minus);
         btn_plus = (Button) findViewById(R.id.btn_plus);
-
+        et_count = (EditText) findViewById(R.id.et_count);
         imageView.setImageResource(R.drawable.noimage);
         tv_title.setText(selectedItems.getTitle());
         tv_desc.setText(selectedItems.getDescription());
@@ -45,19 +46,50 @@ public class DetailItem extends AppCompatActivity {
         btn_plus.setOnClickListener((View v) -> {
             plus();
         });
+        btn_minus.setOnClickListener((View v) -> {
+            minus();
+        });
+        et_count.setText("0");
 
     }
-    public void plus (){
-        if(Store.cartItems.stream().filter(i->i.getItem().getId() == selectedItems.getId()).count() >0){
-            List<CartItem> filteredList  = Store.cartItems.stream().filter(i->i.getItem().getId() == selectedItems.getId()).collect(Collectors.toList());
-            filteredList.get(0).setCount(filteredList.get(0).getCount()+1);
+
+    public void plus() {
+        if (Store.cartItems.stream().filter(i -> i.getItem().getId() == selectedItems.getId()).count() > 0) {
+            List<CartItem> filteredList = Store.cartItems.stream().filter(i -> i.getItem().getId() == selectedItems.getId()).collect(Collectors.toList());
+            filteredList.get(0).setCount(filteredList.get(0).getCount() + 1);
+
+            et_count.setText(Integer.toString(filteredList.get(0).getCount()));
             System.out.println(filteredList.get(0).getCount());
 
-        }else {
+        } else {
             Store.cartItems.add(new CartItem(selectedItems, 1));
-            System.out.println("Adding item to cart");
+//            System.out.println("Adding item to cart");
+            et_count.setText("1");
         }
-        System.out.println("plus");
+//        System.out.println("plus");
+    }
+
+    public void minus() {
+        if (Store.cartItems.stream().filter(i -> i.getItem().getId() == selectedItems.getId()).count() > 0) {
+            List<CartItem> filteredList = Store.cartItems.stream().filter(i -> i.getItem().getId() == selectedItems.getId()).collect(Collectors.toList());
+
+
+            if (filteredList.get(0).getCount() > 1) {
+                filteredList.get(0).setCount(filteredList.get(0).getCount() - 1);
+                et_count.setText(Integer.toString(filteredList.get(0).getCount()));
+//                System.out.println("reduce item by 1");
+            } else{
+                Store.cartItems.remove(filteredList.get(0));
+                et_count.setText("0");
+//                System.out.println("removing item from list");
+            }
+
+            System.out.println(filteredList.get(0).getCount());
+        } else {
+            et_count.setText("0");
+//            System.out.println("no item in cart");
+        }
+//        System.out.println("minus");
     }
 
 //    @Override
