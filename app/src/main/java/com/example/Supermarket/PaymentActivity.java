b81aa5744bb48e65237566997dedcc98597bfa56
pricyWinner <alex.imanuel.28@gmail.com>
2022-06-18 09:06:14 +0700

    add total price di payment page

--- app/src/main/java/com/example/Supermarket/PaymentActivity.java
+++ app/src/main/java/com/example/Supermarket/PaymentActivity.java
@@ -6,24 +6,42 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import Adapters.CartAdapter;
+import Models.CartItem;
+import Services.UserServices;
+import Store.Store;
 
 public class PaymentActivity extends AppCompatActivity {
     Button btn_checkout;
+    TextView tv_totalPrice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
         btn_checkout = (Button) findViewById(R.id.btn_checkout_payment);
+        tv_totalPrice = (TextView) findViewById(R.id.tv_totalPrice);
         RecyclerView recyclerView = findViewById(R.id.recyclerViewpayment);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new CartAdapter());
 
+        List<CartItem> filteredList = Store.cartItems.stream().filter(i -> i.getUserID() == UserServices.currentUser.getUserId()).collect(Collectors.toList());
+        int totalPrice = 0;
+        for (int i = 0; i<filteredList.size(); i++) {
+           totalPrice =   totalPrice + filteredList.get(i).getItem().getPrice() * filteredList.get(i).getCount();
+//            Log.wtf("test", Integer.toString(totalPrice));
+        }
+        tv_totalPrice.setText("Total Price: " + "Rp." + Integer.toString(totalPrice));
         btn_checkout.setOnClickListener((View v) -> {
             Intent intent = new Intent(this, MainActivity.class);
             Toast toast = Toast.makeText(getApplicationContext(), "checkout successful", Toast.LENGTH_SHORT);
